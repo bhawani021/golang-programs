12eb108a9775bcd18f053e8f4a0e8f01279c02f9
Bhawani Shanker <bhawani0915@gmail.com>
2022-04-04 09:43:37 +0530

    Update 0001_rotate_array.go

--- ds/array/0001_rotate_array.go
+++ ds/array/0001_rotate_array.go
@@ -3,27 +3,32 @@ package main
 import "fmt"
 
 func rotateArray(arr []int, d int) {
-	n := len(arr)
-	for i := 0; i < d; i++ {
-		// read first element of array
-		temp := arr[0]
 
-		// let rotate array by one
-		for j := 0; j < n - 1; j++ {
+	// length of array
+	n := len(arr)
+
+	// run a look form 0->d
+	for i := 0; i < d; i++ {
+		// get first value from array
+		val := arr[i]
+
+		// left shift element
+		for j := 0; j < n-1; j++ {
 			arr[j] = arr[j+1]
 		}
 
-		// put first element at end of array
-		arr[n-1] = temp
+		arr[n-1] = val
 	}
 }
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 6}
+	arr := []int{10, 3, -1, 2, 3, 4, 5, 6}
+
 	rotateArray(arr, 3)
 
 	fmt.Println(arr)
 }
 
-// Time complexity : O(n * d) 
-// Auxiliary Space : O(1)
+// output: [2 3 4 5 6 10 -1 3]
+// complexity: O(n*d)
+// Auxiliary space O(1)
